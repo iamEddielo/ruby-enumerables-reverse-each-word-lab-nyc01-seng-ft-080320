f873f1c434a3555bcf628a3535850d6d53eda7c3
Eddie Lopez <iameddielopez@gmail.com>
2020-07-23 16:19:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   new_string = [" "]
   string = string.split(" ")
   new_string << string.each {|string| string.reverse()}
-  new_string.each {|string| string +}
+  new_string.each {|string| p string +}
 
 end
   
